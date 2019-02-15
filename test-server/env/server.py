1c0f2e0508106dd5f8791832bb2ddb98e33ef2ee
jasonfagerberg-toast <jason.fagerberg@toasttab.com>
2019-02-14 21:16:27 -0500

    server noe sends back some mock data and components sitch

--- test-server/env/server.py
+++ test-server/env/server.py
@@ -2,6 +2,8 @@ from flask import render_template, Flask, request
 from werkzeug import secure_filename
 from flask_cors import CORS
 from crossdomain import crossdomain
+from flask import jsonify
+import time
 
 # Create the application instance
 app = Flask(__name__)
@@ -23,10 +25,11 @@ def upload():
 @app.route('/uploader', methods = ['GET', 'POST'])
 @crossdomain(origin='*')
 def upload_file():
-	if request.method == 'POST':
-		f = request.files['file']
-		f.save(secure_filename(f.filename))
-		return 'File uploaded successfully!'
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        time.sleep(5)
+        return jsonify([('abc', 12), ('def', 24), ('ghi', 48)])
 		
 		
 # If we're running in stand alone mode, run the application
